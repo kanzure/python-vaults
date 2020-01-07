ebe9a8b7019e91d5ddcaee0b0c7846d6748e4dda
Bryan Bishop <kanzure@gmail.com>
2020-01-07 15:47:05 -0600

    make a python library

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+import os
+
+from vaults.__version__ import __version__
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, "README.md")) as fd:
+    README = fd.read()
+
+install_requires = [
+    "python-bitcoinlib>=0.10.2",
+]
+
+setup(name="python-vaults",
+      version=__version__,
+      description="Bitcoin cold storage system focused on theft minimization.",
+      long_description=README,
+      long_description_content_type="text/markdown",
+      classifiers=[
+        "Programming Language :: Python",
+      ],
+      url="https://github.com/kanzure/python-vaults",
+      keywords="bitcoin",
+      packages=find_packages(),
+      zip_safe=False,
+      install_requires=install_requires,
+      test_suite="vaults.tests",
+)
---
+++ vaults/__init__.py
@@ -0,0 +1,4 @@
+
+# Note that setup.py can break if __init__.py imports any external
+# dependencies, as these might not be installed when setup.py runs.
+from .__version__ import __version__
