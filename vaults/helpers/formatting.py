bfd8951cac38efdf65dc9c4d720d4ce143c34bc5
Bryan Bishop <kanzure@gmail.com>
2020-01-07 17:20:28 -0600

    some helpers for hex and byte formats

---
+++ vaults/helpers/formatting.py
@@ -0,0 +1,13 @@
+from bitcoin.core import (
+    # convert bytes to hex (see x)
+    b2x,
+
+    # convert hex to bytes (see b2x)
+    x,
+
+    # convert little-endian hex string to bytes (see b2lx)
+    lx,
+
+    # convert bytes to little-endian hex string (see lx)
+    b2lx,
+)
