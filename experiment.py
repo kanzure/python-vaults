3566035b99e04ec4baf226061fa3b370c0703194
Bryan Bishop <kanzure@gmail.com>
2020-01-11 21:06:48 -0600

    experiment: implement some basic unit tests

--- experiment.py
+++ experiment.py
@@ -1,3 +1,10 @@
+"""
+Run the unit tests with:
+    python3 -m unittest experiment.py
+
+"""
+
+import unittest
 import hashlib
 
 import bitcoin
@@ -26,7 +33,8 @@ sys.path.insert(0, "/home/kanzure/local/bitcoin/bitcoin/test/functional")
 from test_framework.test_framework import BitcoinTestFramework
 from test_framework.util import connect_nodes
 
-class VaultsTest(BitcoinTestFramework):
+#class VaultsTest(BitcoinTestFramework):
+class VaultsBlah:
     def set_test_params(self):
         self.setup_clean_chain = True
         self.num_nodes = 2
@@ -192,6 +200,44 @@ class PlannedUTXO(object):
 
         return output
 
+class AbstractPlanningTests(unittest.TestCase):
+    def test_planned_transaction(self):
+        planned_transaction1 = PlannedTransaction(name="name goes here")
+        planned_transaction2 = PlannedTransaction(name="")
+        planned_transaction3 = PlannedTransaction(name=None)
+        del planned_transaction3
+        del planned_transaction2
+        del planned_transaction1
+
+    def test_planned_transaction_counter(self):
+        counter_start = int(PlannedTransaction.__counter__)
+        planned_transaction1 = PlannedTransaction(name="name goes here")
+        planned_transaction2 = PlannedTransaction(name="")
+        planned_transaction3 = PlannedTransaction(name=None)
+        counter_end = int(PlannedTransaction.__counter__)
+
+        self.assertEqual(counter_end - counter_start, 3)
+        del planned_transaction3
+        del planned_transaction2
+        del planned_transaction1
+        self.assertEqual(counter_end - counter_start, 3)
+
+    def test_planned_utxo(self):
+        utxo1 = PlannedUTXO(name="some UTXO", transaction=None, script_description_text="blah")
+        utxo2 = PlannedUTXO(name="second UTXO", transaction=None)
+        utxo3 = PlannedUTXO(name="another UTXO")
+        del utxo3
+        del utxo2
+        del utxo1
+
+    def test_planned_utxo_counter(self):
+        counter_start = int(PlannedUTXO.__counter__)
+        utxo1 = PlannedUTXO(name="some UTXO")
+        counter_end = int(PlannedUTXO.__counter__)
+
+        self.assertEqual(counter_end - counter_start, 1)
+        del utxo1
+        self.assertEqual(counter_end - counter_start, 1)
 
 def make_push_to_cold_storage_transaction(incoming_utxo):
     push_transaction = PlannedTransaction(name="Push (sharded?) UTXO to cold storage wallet")
