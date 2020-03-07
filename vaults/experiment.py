91b926d1175d34102549aaa98362e5788109fd4d
Bryan Bishop <kanzure@gmail.com>
2020-03-07 11:11:18 -0600

    remove direct dependency on test_framework

--- vaults/experiment.py
+++ vaults/experiment.py
@@ -5,6 +5,7 @@ Run the unit tests with:
 """
 
 import os
+import sys
 import uuid
 import unittest
 import hashlib
@@ -34,110 +35,35 @@ import bitcoin.rpc
 
 SelectParams("regtest")
 
+# ----
+#
+# This is from bitcoin's test_framework/messages.py
+# required for ser_string
 
-# TODO: create a python library for bitcoin's test_framework
-import sys
-sys.path.insert(0, "/home/kanzure/local/bitcoin/bitcoin/test/functional")
-from test_framework.test_framework import BitcoinTestFramework
-from test_framework.util import connect_nodes
-from test_framework.messages import ser_string
+def deser_compact_size(f):
+    nit = struct.unpack("<B", f.read(1))[0]
+    if nit == 253:
+        nit = struct.unpack("<H", f.read(2))[0]
+    elif nit == 254:
+        nit = struct.unpack("<I", f.read(4))[0]
+    elif nit == 255:
+        nit = struct.unpack("<Q", f.read(8))[0]
+    return nit
+
+def deser_string(f):
+    nit = deser_compact_size(f)
+    return f.read(nit)
+
+def ser_string(s):
+    return ser_compact_size(len(s)) + s
+
+# ----
 
 TRANSACTION_STORE_FILENAME ="transaction-store.json"
 TEXT_RENDERING_FILENAME = "text-rendering.txt"
 VAULTFILE_FILENAME = "vaultfile"
 
 VAULT_FILE_FORMAT_VERSION = "0.0.1"
-
-#class VaultsTest(BitcoinTestFramework):
-class VaultsBlah:
-    def set_test_params(self):
-        self.setup_clean_chain = True
-        self.num_nodes = 2
-        self.extra_args = [
-            ["-txindex"],
-            ["-txindex"],
-            ["-txindex"],
-        ]
-        self.supports_cli = False
-
-    def setup_network(self):
-        super().setup_network()
-
-        # TODO: verify that these parameters are right
-        connect_nodes(self.nodes[0], 1)
-
-    # TODO: finish implementation of this test. But first implement something
-    # to "fill out" the tree of possible transactions with actual bitcoin
-    # transactions.
-    def run_test(self):
-        self.log.info("Starting the vault test.")
-
-        # mine some initial coins to play around with
-        self.nodes[1].generate(2)
-        self.sync_all()
-        self.nodes[0].generate(110)
-        self.sync_all()
-
-        # options: legacy, p2sh-segwit, bech32
-        address_BECH32 = self.nodes[0].getnewaddress("", "bech32")
-
-        # convert some coins into segwit coins
-        txid = self.nodes[0].sendtoaddress(some_segwit_address, 600)
-
-        # make sure the segwit coins are mature
-        self.nodes[0].generate(6)
-        self.sync_all()
-
-        # isolate the unspent segwit utxo
-        utxos = self.nodes[0].listunspent()
-        segwit_utxo = [{"txid": utxo["txid"], "vout": utxo["vout"]} for utxo in utxos if utxo["txid"] == txid][0]
-
-        # Basic diagram:
-        #
-        # segwit utxo -> 2-of-2 P2WSH utxo -> pre-signed transaction (w/ 100 utxos?)
-        #
-        # Note: this works because the P2WSH utxo has a script spending to an
-        # ephemeral key, and the pre-signed transaction is signed using that
-        # ephemeral key.
-
-        # TODO: create 2 private keys, for the 2-of-2 P2WSH multisig script.
-        # Calculate their public keys. These private keys will be deleted
-        # later.
-        private_key1 = ""
-        private_key2 = ""
-
-
-        # TODO: construct the final 2-of-2 P2WSH multisig script. This script
-        # should only be spendable by one of the previously-created pre-signed
-        # transactions.
-        input_txid = segwit_utxo["txid"]
-        input_txid_vout = segwit_utxo["vout"]
-        output_script = None # TODO: 2-of-2 multisig, spendable by the two private keys
-        (txid1, transaction1) = createrawtransaction(inputs, outputs)
-
-        # Make a pre-signed transaction that spends the 2-of-2 PWSH utxo txid,
-        # and creates 100 UTXOs. Delete the ephemeral key after signing.
-        input_txid2 = txid1
-        input_txid2_vout = 0
-        outputs2 = None # TODO: make 100 outputs, each with a similar script
-        (txid2, transaction2) = createrawtransaction(inputs2, outputs2)
-        # TODO: sign transaction2, using the two private keys
-
-
-        # For each UTXO, make: (1) a push to cold storage transaction, (2) a
-        # push to new identical vault (using same keys).
-        for utxo in created_utxos:
-            make_push_to_cold_storage_transaction_for_utxo(utxo)
-            make_push_to_vault_transaction_for_utxo(utxo, splitting=False)
-
-        # TODO: sign the tree of transactions (in any order)
-
-        # delete the private keys
-        del private_key1
-        del private_key2
-
-        # Now sign the transaction that starts the whole thing.
-        self.nodes[0].signrawtransaction(transaction1)
 
 class ScriptTemplate(object):
 
---
+++ vaults/tests/bitcointestframework.py
@@ -0,0 +1,102 @@
+"""
+This file describes an unused test. Could be deleted.
+"""
+
+# TODO: create a python library for bitcoin's test_framework
+import sys
+sys.path.insert(0, "/home/kanzure/local/bitcoin/bitcoin/test/functional")
+
+from test_framework.test_framework import BitcoinTestFramework
+from test_framework.util import connect_nodes
+from test_framework.messages import ser_string
+
+class VaultsTest(BitcoinTestFramework):
+    def set_test_params(self):
+        self.setup_clean_chain = True
+        self.num_nodes = 2
+        self.extra_args = [
+            ["-txindex"],
+            ["-txindex"],
+            ["-txindex"],
+        ]
+        self.supports_cli = False
+
+    def setup_network(self):
+        super().setup_network()
+
+        # TODO: verify that these parameters are right
+        connect_nodes(self.nodes[0], 1)
+
+    # TODO: finish implementation of this test. But first implement something
+    # to "fill out" the tree of possible transactions with actual bitcoin
+    # transactions.
+    def run_test(self):
+        self.log.info("Starting the vault test.")
+
+        # mine some initial coins to play around with
+        self.nodes[1].generate(2)
+        self.sync_all()
+        self.nodes[0].generate(110)
+        self.sync_all()
+
+        # options: legacy, p2sh-segwit, bech32
+        address_BECH32 = self.nodes[0].getnewaddress("", "bech32")
+
+        # convert some coins into segwit coins
+        txid = self.nodes[0].sendtoaddress(some_segwit_address, 600)
+
+        # make sure the segwit coins are mature
+        self.nodes[0].generate(6)
+        self.sync_all()
+
+        # isolate the unspent segwit utxo
+        utxos = self.nodes[0].listunspent()
+        segwit_utxo = [{"txid": utxo["txid"], "vout": utxo["vout"]} for utxo in utxos if utxo["txid"] == txid][0]
+
+        # Basic diagram:
+        #
+        # segwit utxo -> 2-of-2 P2WSH utxo -> pre-signed transaction (w/ 100 utxos?)
+        #
+        # Note: this works because the P2WSH utxo has a script spending to an
+        # ephemeral key, and the pre-signed transaction is signed using that
+        # ephemeral key.
+
+        # TODO: create 2 private keys, for the 2-of-2 P2WSH multisig script.
+        # Calculate their public keys. These private keys will be deleted
+        # later.
+        private_key1 = ""
+        private_key2 = ""
+
+
+        # TODO: construct the final 2-of-2 P2WSH multisig script. This script
+        # should only be spendable by one of the previously-created pre-signed
+        # transactions.
+        input_txid = segwit_utxo["txid"]
+        input_txid_vout = segwit_utxo["vout"]
+        output_script = None # TODO: 2-of-2 multisig, spendable by the two private keys
+        (txid1, transaction1) = createrawtransaction(inputs, outputs)
+
+        # Make a pre-signed transaction that spends the 2-of-2 PWSH utxo txid,
+        # and creates 100 UTXOs. Delete the ephemeral key after signing.
+        input_txid2 = txid1
+        input_txid2_vout = 0
+        outputs2 = None # TODO: make 100 outputs, each with a similar script
+        (txid2, transaction2) = createrawtransaction(inputs2, outputs2)
+        # TODO: sign transaction2, using the two private keys
+
+        # For each UTXO, make: (1) a push to cold storage transaction, (2) a
+        # push to new identical vault (using same keys).
+        for utxo in created_utxos:
+            make_push_to_cold_storage_transaction_for_utxo(utxo)
+            make_push_to_vault_transaction_for_utxo(utxo, splitting=False)
+
+        # TODO: sign the tree of transactions (in any order)
+
+        # delete the private keys
+        del private_key1
+        del private_key2
+
+        # Now sign the transaction that starts the whole thing.
+        self.nodes[0].signrawtransaction(transaction1)
+
+
