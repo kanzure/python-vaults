baeb64788fe4a3865dfe3496ff9f454aacfb3a7b
Bryan Bishop <kanzure@gmail.com>
2020-03-08 16:44:06 -0500

    fix ser_string implementation

--- vaults/utils.py
+++ vaults/utils.py
@@ -6,19 +6,17 @@ import hashlib
 # This is from bitcoin's test_framework/messages.py
 # required for ser_string
 
-def deser_compact_size(f):
-    nit = struct.unpack("<B", f.read(1))[0]
-    if nit == 253:
-        nit = struct.unpack("<H", f.read(2))[0]
-    elif nit == 254:
-        nit = struct.unpack("<I", f.read(4))[0]
-    elif nit == 255:
-        nit = struct.unpack("<Q", f.read(8))[0]
-    return nit
-
-def deser_string(f):
-    nit = deser_compact_size(f)
-    return f.read(nit)
+def ser_compact_size(l):
+    r = b""
+    if l < 253:
+        r = struct.pack("B", l)
+    elif l < 0x10000:
+        r = struct.pack("<BH", 253, l)
+    elif l < 0x100000000:
+        r = struct.pack("<BI", 254, l)
+    else:
+        r = struct.pack("<BQ", 255, l)
+    return r
 
 def ser_string(s):
     return ser_compact_size(len(s)) + s
